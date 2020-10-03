dd5fd5dbbcf4aedf272b92d307443acd9d0472c6
jackieinginfo <43906427+jackieinginfo@users.noreply.github.com>
2020-10-03 18:47:13 +0200

    Add Loggin fragment_e_commerce_login.xml Join ActivityMain ToDo Intent

--- app/src/main/java/com/example/ecommerce/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.ecommerce
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
+import android.widget.Toast.*
+import com.example.ecommerce.ui.login.eCommerceLogin
+import kotlinx.android.synthetic.main.fragment_e_commerce_login.view.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var login: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login = findViewById(R.id.logg)
+
+        login.setOnClickListener{
+            val intent = Intent(this@MainActivity, eCommerceLogin::class.java)
+        }
+        //loginBtn= findViewById(R.id.main_login_bt)
+
+
+
+
     }
 }
