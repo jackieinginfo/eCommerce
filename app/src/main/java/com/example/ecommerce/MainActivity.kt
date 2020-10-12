77aa331bb1cd54a1c2940de3e22922c5d7f4e68d
jackieinginfo <43906427+jackieinginfo@users.noreply.github.com>
2020-10-13 00:04:23 +0200

    Ajust ItemUnitAdapter and add Icon

--- app/src/main/java/com/example/ecommerce/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.example.ecommerce
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.Toast
-import android.widget.Toast.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.ecommerce.ui.login.eCommerceLogin
 import com.smarteist.autoimageslider.SliderAnimations
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_e_commerce_login.view.*
 
 
 class MainActivity : AppCompatActivity()
