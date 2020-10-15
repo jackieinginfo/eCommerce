6b194fbd01fabf100b4e3763ec8f27639a7e2847
jackieinginfo <43906427+jackieinginfo@users.noreply.github.com>
2020-10-15 02:49:46 +0200

    Change Linear Layout to Layout on activity_main.xml

--- app/src/main/java/com/example/ecommerce/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce/MainActivity.kt
@@ -1,23 +1,29 @@
 package com.example.ecommerce
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.smarteist.autoimageslider.SliderAnimations
+import androidx.databinding.DataBindingUtil
+import com.example.ecommerce.databinding.ActivityMainBinding
+import com.example.ecommerce.ui.login.eCommerceLogin
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity()
 {
-    private lateinit var login: Button
+    private lateinit var activitiesBinding: ActivityMainBinding
+    private  val ecommercelogin: eCommerceLogin = eCommerceLogin()
+    private val newWordActivityRequestCode = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activitiesBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //EcommerceLogin
 
 
-
-
+        //autoSlider
         val imageSliderAdapter = ImageSliderAdapter(this)
         main_image_android.setSliderAdapter(imageSliderAdapter)
         main_image_android.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION)
@@ -43,6 +49,8 @@ class MainActivity : AppCompatActivity()
         //https://images.pexels.com/photos/4110012/pexels-photo-4110012.jpeg?cs=srgb&dl=pexels-polina-tankilevitch-4110012.jpg&fm=jpg
         items.add("https://images.pexels.com/photos/3905697/pexels-photo-3905697.jpeg?cs=srgb&dl=pexels-anna-shvets-3905697.jpg&fm=jpg")
         items.add("https://images.pexels.com/photos/2680603/pexels-photo-2680603.jpeg?cs=srgb&dl=pexels-anna-tukhfatullina-food-photographerstylist-2680603.jpg&fm=jpg")
+        items.add("https://images.pexels.com/photos/1070850/pexels-photo-1070850.jpeg?cs=srgb&dl=pexels-daria-shevtsova-1070850.jpg&fm=jpg")
+        items.add("https://images.pexels.com/photos/3992381/pexels-photo-3992381.jpeg?cs=srgb&dl=pexels-cottonbro-3992381.jpg&fm=jpg")
         items.add("https://images.pexels.com/photos/4686833/pexels-photo-4686833.jpeg?cs=srgb&dl=pexels-cottonbro-4686833.jpg&fm=jpg")
         return items
     }
--- app/src/main/java/com/example/ecommerce/ui/login/ECommerceLogin.kt
+++ app/src/main/java/com/example/ecommerce/ui/login/ECommerceLogin.kt
@@ -15,7 +15,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
-
 import com.example.ecommerce.R
 
 class eCommerceLogin : Fragment() {
